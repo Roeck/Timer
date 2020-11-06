ffd3f1b2ce7df1f7332847a58003f32cef493ac3
Roeck <rons9n@gmail.com>
2020-11-05 19:25:53 -0500

    calculateTimer function

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css'
 
 function Main() {
+    const [timeInSeconds, setTimeInSeconds] = useState<number>(0);
     return (
         <section className="time-container">
             <p className="timer-text">0</p>

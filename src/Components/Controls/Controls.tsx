afffc2e546f2702f290f290615fe5e0870c08813
Roeck <rons9n@gmail.com>
2020-11-05 19:13:17 -0500

    initial structure with main and controls compoments and styles

---
+++ src/Components/Controls/Controls.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Controls() {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default Controls;
---
+++ src/Components/Main/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css'
+
+function Main() {
+    return (
+        <section className="time-container">
+            <p className="timer-text">0</p>
+            <span>:</span>
+            <p className="timer-text">0</p>
+            <span>:</span>
+            <p className="timer-text">0</p>
+        </section>
+    )
+}
+
+export default Main;
